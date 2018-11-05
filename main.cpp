baef104d63dfed5c9eacddf571c628fde21f129d
LoreDewulf <44776033+LoreDewulf@users.noreply.github.com>
2018-11-05 15:15:36 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include "Vec.cpp"
+using namespace std;
+
+
+int main(){
+	return 0;};
